f5a3c319ec5630dae315dedade63f98881971d35
lord-aceldama <aceldama.v1.0@gmail.com>
2022-04-12 05:24:59 +0100

    fleshed out run scripts

--- run.ps1
+++ run.ps1
@@ -1,3 +1,14 @@
+if (![System.IO.Directory]::Exists("venv")) {
+    Write-Host "creating virtual environment..."
+    python3 -m venv venv
+
+    Write-Host "installing dependencies..."
+    .\venv\scripts\activate
+    pip3 install -r requirements.txt
+    deactivate
+}
+
+Write-Host "starting snek..."
 [Console]::CursorVisible = $false
 .\venv\Scripts\python.exe snake.py
 [Console]::CursorVisible = $true
