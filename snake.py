bc5debb43373dfc250d259440af7149f0fa4d8fd
AceldamA <lord-aceldama@users.noreply.github.com>
2022-04-12 03:22:14 +0100

    Update snake.py
    optimised code and used ansi codes to fix display flickering

--- snake.py
+++ snake.py
@@ -1,87 +1,92 @@
-from pytimedinput import timedInput
 from random import randint
-import os
 from colorama import Fore, init
+from pytimedinput import timedInput
 
-def print_field():
-	for cell in CELLS:
-		if cell in snake_body:
-			print(Fore.GREEN + 'X', end = '')
-		elif cell == apple_pos:
-			print(Fore.RED + 'a',end = '')
-		elif cell[1] in (0, FIELD_HEIGHT - 1) or cell[0] in (0, FIELD_WIDTH - 1):
-			print(Fore.CYAN + '#', end = '')
-		else:
-			print(' ', end = '')
-
-		if cell[0] == FIELD_WIDTH - 1:
-			print('')
-
-def update_snake():
-	global eaten
-	new_head = snake_body[0][0] + direction[0], snake_body[0][1] + direction[1]
-	snake_body.insert(0,new_head)
-	if not eaten:
-		snake_body.pop(-1)
-	eaten = False
-
-def apple_collision():
-	global apple_pos, eaten
-
-	if snake_body[0] == apple_pos:
-		apple_pos = place_apple()
-		eaten = True
-
-def place_apple():
-	col = randint(1,FIELD_WIDTH - 2)
-	row = randint(1,FIELD_HEIGHT - 2)
-	while (col, row) in snake_body:
-		col = randint(1,FIELD_WIDTH - 2)
-		row = randint(1,FIELD_HEIGHT - 2)
-	return (col,row)
-
-init(autoreset=True)
 
 # settings
 FIELD_WIDTH = 32
 FIELD_HEIGHT = 16
-CELLS = [(col,row) for row in range(FIELD_HEIGHT) for col in range(FIELD_WIDTH)]
+SPEED = 0.3
+DIRECTIONS = {'w': (0, -1), 'a': (-1, 0), 's': (0, 1), 'd': (1, 0)}
 
-# snake
-snake_body = [
-	(5,FIELD_HEIGHT // 2),
-	(4,FIELD_HEIGHT // 2),
-	(3,FIELD_HEIGHT // 2)]
-DIRECTIONS = {'left':(-1,0),'right': (1,0),'up': (0,-1),'down': (0,1)}
-direction = DIRECTIONS['right']
-eaten = False
+#ansi
+CLS = "2J"
+GOTO00 = "H"
+CURSOR_ON = "?25h"
+CURSOR_OFF = "?25l"
+
+
+def ansi(*code: str):
+    """ See: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797 """
+    for c in code:
+        print(f"\033[{c}", end="")
+
+
+def is_border(x, y):
+    return ((x % FIELD_WIDTH) * (y % FIELD_HEIGHT)) == 0
+
+
+def print_field():
+    ansi(GOTO00)
+    row = 0
+    while row <= FIELD_HEIGHT:
+        col = 0
+        while col <= FIELD_WIDTH:
+            cell = col, row
+            if is_border(col, row):
+                print(Fore.CYAN + '#', end='')
+            elif cell in snake_body:
+                print(Fore.GREEN + 'X', end='')
+            elif cell == apple_pos:
+                print(Fore.RED + 'a', end='')
+            else:
+                print(' ', end='')
+            col += 1
+        row += 1
+        print()
+
+
+def place_apple():
+    while True:
+        col = randint(1, FIELD_WIDTH - 2)
+        row = randint(1, FIELD_HEIGHT - 2)
+        if (col, row) not in snake_body:
+            return col, row
+
+
+def update_snake(apple_xy):
+    new_head = snake_body[0][0] + direction[0], snake_body[0][1] + direction[1]
+    snake_body.insert(0, new_head)
+    if apple_xy != new_head:
+        snake_body.pop(-1)
+    else:
+        apple_xy = place_apple()
+    return apple_xy
+
+
+# init snek
+snake_body = [(5 - x, FIELD_HEIGHT // 2) for x in range(3)]
+direction = DIRECTIONS['d']
 apple_pos = place_apple()
 
+init(autoreset=True)
+ansi(CLS, CURSOR_OFF)
 while True:
-	# clear field
-	os.system('cls')
-	
-	# draw field
-	print_field()
+    # draw field
+    print_field()
 
-	# get input
-	txt,_ = timedInput('',timeout = 0.3)
-	match txt:
-		case 'w': direction = DIRECTIONS['up']
-		case 'a': direction = DIRECTIONS['left']
-		case 's': direction = DIRECTIONS['down']
-		case 'd': direction = DIRECTIONS['right']
-		case 'q':
-			os.system('cls')
-			break
+    # get input
+    txt, _ = timedInput("   \r", SPEED)
+    if txt and (txt in 'wasd'):
+        direction = DIRECTIONS[txt]
+    elif txt == 'q':
+        break
 
-	# update game 
-	update_snake()
-	apple_collision()
+    # update game
+    apple_pos = update_snake(apple_pos)
 
-	# check death
-	if snake_body[0][1] in (0, FIELD_HEIGHT - 1) or \
-	   snake_body[0][0] in (0,FIELD_WIDTH - 1) or\
-	   snake_body[0] in snake_body[1:]:
-		os.system('cls')
-		break
+    # check death
+    if is_border(*snake_body[0]) or snake_body[0] in snake_body[1:]:
+        break
+
+ansi(CLS, CURSOR_ON)
